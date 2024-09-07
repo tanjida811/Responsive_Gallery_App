1745361268bb7794593af2774433fa3149abcd8b
Tanjida-aktar <tanjidaaktar2003@gmail.com>
2024-09-08 00:54:29 +0600

    first commit

--- lib/image_show_screen.dart
+++ lib/image_show_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gallery_app/gallery_screen.dart';
 
 class ImageShowScreen extends StatelessWidget {
  final String imageUrl;
@@ -8,6 +9,15 @@ class ImageShowScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        leading:IconButton (
+          onPressed: (){
+            Navigator.push(context,MaterialPageRoute(builder: (context) => GalleryScreen(),),);
+          },
+          icon: Icon(Icons.arrow_back),
+        )
+      ),
       body: Image.network(
           imageUrl,
           height: double.infinity,
